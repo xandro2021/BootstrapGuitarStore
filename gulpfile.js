31fbf95c7a68b4c2acc4631f14bcd9edf66988a0
xandro2021 <xandrocastro2021@gmail.com>
2023-04-03 12:39:31 -0600

    Purge the unused bootstrap css/scss

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ function cssbuild( done ) {
             suffix: '.min'
         }))
         .pipe( purgecss({
-            content: ['index.html']
+            content: ['index.html', 'base.html', 'blog.html', 'entrada.html', 'nosotros.html', 'producto.html', 'tienda.html']
         }))
         .pipe( dest('build/css'))
 
